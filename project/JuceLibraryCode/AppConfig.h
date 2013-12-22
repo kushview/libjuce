ec8485f428bb27a58da1ac5f44c73a7196ea9923
Michael R. Fisher <mfisher31@gmail.com>
2013-12-22 03:01:52 -0600

    Updating Introjucer project. Removing 'Builds' directory

--- project/JuceLibraryCode/AppConfig.h
+++ project/JuceLibraryCode/AppConfig.h
@@ -169,6 +169,10 @@
  //#define JUCE_WEB_BROWSER
 #endif
 
+#ifndef    JUCE_ENABLE_LIVE_CONSTANT_EDITOR
+ //#define JUCE_ENABLE_LIVE_CONSTANT_EDITOR
+#endif
+
 //==============================================================================
 // juce_video flags:
 
