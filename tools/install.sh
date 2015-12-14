03953d485d9f94af2ffc7f0a7820c3e3fabaac08
Michael R. Fisher <mfisher31@gmail.com>
2015-12-14 00:45:32 -0500

    Touch up install script

--- tools/install.sh
+++ tools/install.sh
@@ -3,6 +3,6 @@
 set -ex
 ./waf configure clean build --introjucer --juce-demo
 ./waf install
-./waf clean configure --debug
+./waf clean configure build --debug
 ./waf install --no-headers
 ./waf distclean
