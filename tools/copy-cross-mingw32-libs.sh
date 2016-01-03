35fe6e9449034e6a723fd9df5fa45bc03b817765
Michael R. Fisher <mfisher31@gmail.com>
2016-01-03 07:45:04 -0500

    Added destdir parameter to dll copy script

--- tools/copy-cross-mingw32-libs.sh
+++ tools/copy-cross-mingw32-libs.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-set -ex
+destdir="$1"
+if [ -z "$destdir" ]; then
+  destdir="`pwd`/build"
+fi
+
+set -e
 dllroot=/usr/lib/gcc/i686-w64-mingw32/4.9-win32
-cp "${dllroot}/libgcc_s_sjlj-1.dll" build/
-cp "${dllroot}/libstdc++-6.dll" build/
-chmod +x build/*.dll
+cp -f "${dllroot}/libgcc_s_sjlj-1.dll" "${destdir}/"
+cp -f "${dllroot}/libstdc++-6.dll" "${destdir}/"
+
 exit 0
