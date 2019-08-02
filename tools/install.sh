1cd4c49f7083f0d30c0297186bf14770eca49fd1
Michael Fisher <mfisher@kushview.net>
2019-08-02 11:04:33 -0400

    install.sh now accepts user args

--- tools/install.sh
+++ tools/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -ex
-# ./waf configure clean build --introjucer --juce-demo
+
 ./waf distclean
-./waf configure build install
-./waf clean configure build --debug
+./waf configure build install "$@"
+./waf clean configure build --debug "$@"
 ./waf install --no-headers
 ./waf distclean
